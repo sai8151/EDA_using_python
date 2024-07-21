3b9026f263e9c316438535b56f0c2edf7647312f
sai8151 <saikiranreddy8151@gmail.com>
2024-07-21 16:40:13 +0530

    #2

--- app.py
+++ app.py
@@ -5,3 +5,16 @@ import seaborn as sns
 #to ignore warnings
 import warnings
 warnings.filterwarnings('ignore')
+
+population_data = pd.read_csv('global_air_pollution_data.csv')
+pollution_data = pd.read_csv('world_population.csv')
+
+# Inspect population data
+print(population_data.head())
+print(population_data.info())
+print(population_data.describe())
+
+# Inspect pollution data
+print(pollution_data.head())
+print(pollution_data.info())
+print(pollution_data.describe())
